7675961aca546de1d46416c07cacbbb30a654d15
Micah Akpan <mickey4sure2k7@gmail.com>
2018-12-14 17:28:18 +0100

    Add script to sign up page

--- UI/js/base.js
+++ UI/js/base.js
@@ -4,15 +4,15 @@ let passwordVisible = false;
 for (let i = 0; i < pToggleBtns.length; i++) {
   const toggleBtn = pToggleBtns[i];
   toggleBtn.setAttribute('p-visible', passwordVisible);
-  toggleBtn.onclick = function() {
-      const inputEl = this.parentElement.querySelector('input');
-      
-      if (inputEl.type === 'password') {
-        inputEl.type = 'text';
-        this.textContent = 'hide';
-      } else {
-        inputEl.type = 'password';
-        this.textContent = 'show';
-      }
+  toggleBtn.onclick = function () {
+    const inputEl = this.parentElement.querySelector('input');
+
+    if (inputEl.type === 'password') {
+      inputEl.type = 'text';
+      this.textContent = 'hide';
+    } else {
+      inputEl.type = 'password';
+      this.textContent = 'show';
     }
+  }
 }
