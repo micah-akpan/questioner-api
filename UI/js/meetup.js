079bbd7a6e79f74082cdfe5ee8740d158ab7f291
Micah Akpan <mickey4sure2k7@gmail.com>
2018-12-15 17:26:56 +0100

    Add interactiveness to Search Bar Icon

--- UI/js/meetup.js
+++ UI/js/meetup.js
@@ -1,6 +1,23 @@
-const searchIcon = document.getElementById("search-icon");
-const searchBar = document.getElementById("search-bar");
+const searchIcon = document.getElementById('search-icon');
+const searchBar = document.getElementById('search-bar');
 
-searchIcon.onclick = function() {
+searchIcon.onclick = () => {
+  // TODO: Remove inline styling
+  searchBar.style.width = '300px';
   searchBar.style.display = 'inline-block';
+}
+
+// close search bar
+document.addEventListener('keydown', (e) => {
+hideSearchBar(e.key);
+})
+
+function hideSearchBar(keyPressed) {
+  if (keyPressed === 'Escape') {
+    if (searchBar.style.display === 'inline-block') {
+      searchBar.style.display = 'none';
+    } else {
+      return;
+    }
+  }
 }
