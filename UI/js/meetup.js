4927f6241659fc483b974401240122d60e6ae3fb
Micah Akpan <mickey4sure2k7@gmail.com>
2018-12-15 21:23:56 +0100

    Add a dropdown menu

--- UI/js/meetup.js
+++ UI/js/meetup.js
@@ -1,6 +1,14 @@
 const searchIcon = document.getElementById('search-icon');
 const searchBar = document.getElementById('search-bar');
 
+const btnTrigger = document.querySelector('.dropdown-trigger-btn');
+const dropDownMenu = document.querySelector('.q-user-profile__dropdown-menu');
+
+// Toggle display of dropdown menu
+btnTrigger.onclick = function () {
+  dropDownMenu.style.display === 'none' ? dropDownMenu.style.display = 'block' : dropDownMenu.style.display = 'none';
+}
+
 searchIcon.onclick = () => {
   // TODO: Remove inline styling
   searchBar.style.width = '300px';
@@ -9,15 +17,19 @@ searchIcon.onclick = () => {
 
 // close search bar
 document.addEventListener('keydown', (e) => {
-hideSearchBar(e.key);
+  hideSearchBar(e.key);
 })
 
 function hideSearchBar(keyPressed) {
   if (keyPressed === 'Escape') {
-    if (searchBar.style.display === 'inline-block') {
+    if (searchBar.style.display === 'inline-block' || dropDownMenu.style.display === 'block') {
       searchBar.style.display = 'none';
+      dropDownMenu.style.display = 'none';
     } else {
       return;
     }
   }
 }
+
+document.addEventListener('click', (e) => {
+})
