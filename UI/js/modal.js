e6b0b1ca8a04984f29c5d0f775d44b8acffc2135
Micah Akpan <mickey4sure2k7@gmail.com>
2018-12-17 12:36:53 +0100

    Add a post question UI

--- UI/js/modal.js
+++ UI/js/modal.js
@@ -1,6 +1,18 @@
 const modalCloseBtn = document.getElementById('close-modal-btn');
 const modal = document.getElementById('modal');
 
-modalCloseBtn.onclick = function(e) {
+function hideModal(modal) {
   modal.classList.add('close-modal');
+}
+
+document.onkeydown = function (e) {
+  if (e.key === 'Escape') {
+    hideModal(modal);
+  }
+}
+
+modalCloseBtn.onclick = function (e) {
+  if (!modal.classList.contains('close-modal')) {
+    modal.classList.add('close-modal');
+  }
 }
