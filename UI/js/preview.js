0eaffdb8aeefa26b48ec0f40bfa484b0a802897d
Micah Akpan <mickey4sure2k7@gmail.com>
2018-12-18 12:22:28 +0100

    Add image preview

--- UI/js/preview.js
+++ UI/js/preview.js
@@ -3,13 +3,22 @@ window.onload = (e) => {
   const imgUploadTrigger = d.querySelector('.inner-upload__block');
   const imgUpload = d.querySelector('.q-form__image-upload');
   const imgBlock = d.querySelector('.outer-upload__block');
+  const imgUploadBtns = d.querySelector('.image-upload-btns');
 
-  imgUpload.onchange = function(e) {
+  imgUploadTrigger.onclick = (e) => {
+    // imgUpload.click();
+  }
+
+  imgUpload.onchange = (e) => {
     const file = URL.createObjectURL(e.target.files[0]);
     const uploadedImg = d.createElement('img');
     uploadedImg.src = file;
     uploadedImg.classList.add('upload-image-preview');
     imgBlock.innerHTML = "";
     imgBlock.appendChild(uploadedImg);
+
+    // uploaded image can be further saved
+    // or removed
+    imgUploadBtns.classList.add('image-upload-btns-show');
   }
 }
