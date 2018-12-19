e75c96f59d4ff87a4999f95530a1c611a3ba796f
Micah Akpan <mickey4sure2k7@gmail.com>
2018-12-19 18:01:28 +0100

    Reset the menu trigger icon to its initial state

--- UI/js/mobile-nav.js
+++ UI/js/mobile-nav.js
@@ -1,21 +1,14 @@
 window.onload = (e) => {
   const d = document;
   const qNavMenuWrapper = d.querySelector('.sidebar-menu__wrapper');
-  // const closeNavBtn = d.querySelector('#mobile-nav-sidebar__close-btn');
 
   const qNavMenuIconWrapper = document.querySelector('#mobile-nav-sidebar__wrapper');
 
-  // qNavWrapper.onclick = (e) => {
-  //   qNavMenuWrapper.classList.add('nav-menu-show');
-  // }
-
-  // closeNavBtn.onclick = (e) => {
-  //   qNavMenuWrapper.classList.remove('nav-menu-show');
-  // }
-
   document.onkeydown = (e) => {
     if (e.key === 'Escape') {
-      qNavMenuWrapper.classList.remove('nav-menu-show');
+      qNavMenuWrapper.classList.remove('nav-menu-show')
+      ;
+      qNavMenuIconWrapper.classList.toggle('change');
     }
   }
 
