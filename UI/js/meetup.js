442faab12c416cbd4193b035c97f28d008121c11
Micah Akpan <mickey4sure2k7@gmail.com>
2018-12-20 15:34:41 +0100

    Add image tag feature

--- UI/js/meetup.js
+++ UI/js/meetup.js
@@ -8,6 +8,36 @@ fileInput.onchange = (e) => {
   const image = new Image();
   const imageUrl = URL.createObjectURL(e.target.files[0]);
   image.setAttribute('src', imageUrl);
-  
+
   mPhotosWrapper.appendChild(image);
+}
+
+// Tags
+const addTagBtn = d.querySelector('.btn__tag');
+const tagField = d.querySelector('input[id="tag"]');
+const addTagsContainer = d.querySelector('.meetup-tags-added');
+
+let tags = null;
+
+addTagBtn.onclick = (e) => {
+  // allowed separators for tags are commas and hashes (#)
+  tags = tagField.value.split(",");
+  if (tags.length === 1 && tags[0].includes('#')) {
+    tags = tags[0].split('#');
+  } else {
+    // user probably entered a single value
+    tags = tags;
+  }
+
+  addTagsContainer.innerHTML = "";
+  tags.forEach((tag, i) => {
+    if (tag !== '') {
+      const span = d.createElement('span');
+      
+      // to support deleting the tags later
+      span.id = i;
+      span.textContent = `#${tag}`;
+      addTagsContainer.appendChild(span);
+    }
+  })
 }
