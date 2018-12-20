4b6b1ec6491be2f79eb494a58d40f1e70ad8215c
Micah Akpan <mickey4sure2k7@gmail.com>
2018-12-20 14:06:13 +0100

    Add meetup image upload feature for admins

--- UI/js/meetup.js
+++ UI/js/meetup.js
@@ -1,54 +1,13 @@
-const searchIcon = document.getElementById('search-icon');
-const searchBar = document.getElementById('search-bar');
-
-const btnTrigger = document.querySelector('.dropdown-trigger-btn');
-const dropDownMenu = document.querySelector('.q-user-profile__dropdown-menu');
-
-// Toggle display of dropdown menu
-btnTrigger.onclick = function () {
-  dropDownMenu.style.display === 'none' ? dropDownMenu.style.display = 'block' : dropDownMenu.style.display = 'none';
-}
-
-searchIcon.onclick = () => {
-  searchBar.classList.add('show');
-}
-
-// close search bar
-document.addEventListener('keydown', (e) => {
-  hideSearchBar(e.key);
-})
-
-function hideSearchBar(keyPressed) {
-  if (keyPressed === 'Escape') {
-    searchBar.classList.remove('show')
-  }
-}
-
 const d = document;
 
-const meetupDropdownTrigger = d.querySelector('.q-card__primary-options');
-const meetupDropdownMenu = d.querySelector('.q-card__primary-options .dropdown-menu');
-const delBtn = d.querySelector('.dropdown-menu .delete-option');
-const editBtn = d.querySelector('.dropdown-menu .edit-option');
-const modal = d.querySelector('.modal');
-const closeModalBtn = d.querySelector('.close-modal-btn');
+const mPhotosWrapper = d.querySelector('.meetup-photos__wrapper');
+const uploadBtn = d.querySelector('label[role="button"]');
+const fileInput = d.querySelector('input[type="file"]');
 
-
-meetupDropdownTrigger.onclick = (e) => {
-  meetupDropdownMenu.classList.toggle('active');
-}
-
-delBtn.onclick = (e) => {
-  // pop open delete modal
-  modal.classList.toggle('active');
-}
-
-closeModalBtn.onclick = (e) => {
-  modal.classList.toggle('active');
-}
-
-document.onkeydown = (e) => {
-  if (e.key === 'Escape') {
-    modal.classList.toggle('active');
-  }
+fileInput.onchange = (e) => {
+  const image = new Image();
+  const imageUrl = URL.createObjectURL(e.target.files[0]);
+  image.setAttribute('src', imageUrl);
+  
+  mPhotosWrapper.appendChild(image);
 }
