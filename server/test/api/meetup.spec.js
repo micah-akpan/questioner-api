c7eb0424694090e3fa61c556035e8106f4be61f0
Micah Akpan <mickey4sure2k7@gmail.com>
2018-12-24 22:39:37 +0100

    Remove skipped tests

--- server/test/api/meetup.spec.js
+++ server/test/api/meetup.spec.js
@@ -186,29 +186,4 @@ describe('Meetups API', () => {
         .expect(200, done);
     });
   });
-
-  describe.skip('POST /api/v1/meetups/<meetup-id>/rsvps', () => {
-    it('should rsvp a user', (done) => {
-      agent
-        .post('/api/v1/meetups/2/rsvps')
-        .expect(201)
-        .end((err, res) => {
-          if (err) return done(err);
-          res.body.status.should.equal(201);
-          res.body.data[0].should.have.property('topic');
-          done();
-        });
-    });
-
-    it('should not rsvp a user on a non-existent meetup', (done) => {
-      agent
-        .post('/api/v1/meetups/999999999999999/rsvps')
-        .expect(404)
-        .end((err, res) => {
-          if (err) return done(err);
-          res.body.status.should.equal(404);
-          done();
-        });
-    });
-  });
 });
