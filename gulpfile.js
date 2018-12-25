022f208614e7fe7863dd6630460edeb5bbc17832
Micah Akpan <mickey4sure2k7@gmail.com>
2018-12-25 11:04:58 +0100

    Replace --compilers with --require

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,8 @@ gulp.task('mocha', () => {
   gulp.src(['./server/test/**'], { read: false })
     .pipe(mocha({
       reporter: 'list',
-      compilers: '@babel/register',
-      exit: true
+      exit: true,
+      require: '@babel/register'
     }))
     .on('error', log.error);
 });
