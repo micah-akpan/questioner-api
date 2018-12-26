29e6d5d9b2464bb645a73da43fe52cf184709dcb
Micah Akpan <mickey4sure2k7@gmail.com>
2018-12-26 02:26:58 +0100

    Add new tests

--- server/test/controllers/meetup.spec.js
+++ server/test/controllers/meetup.spec.js
@@ -2,26 +2,91 @@ import 'chai/register-should';
 import sinon from 'sinon';
 import meetupController from '../../controllers/meetup';
 
-describe('Meetup Controller', () => {
-  describe('#getUpcomingMeetups', () => {
-    it('should be a function', () => {
-      meetupController.getUpcomingMeetups.should.be.a('function');
-    });
-
-    it('should fetch upcoming meetups', () => {
-      const res = {};
+describe('Meetups API', () => {
+  describe('Get all meetups', () => {
+    it('should fetch meetups', () => {
+      const res = {
+        status() { },
+        send() { }
+      };
       const req = {};
 
-      const sendStub = sinon.stub().returns(res);
-      const statusStub = sinon.stub().returns(res);
+      res.status = sinon.stub(res, 'status').returns(res);
+      res.send = sinon.stub(res, 'send').returns(res);
 
-      res.status = statusStub;
-      res.send = sendStub;
-
-      meetupController.getUpcomingMeetups(req, res);
+      meetupController.getAllMeetups(req, res);
 
       res.status.calledOnce.should.be.true;
       res.send.calledOnce.should.be.true;
+
+      res.status.firstCall.args[0].should.equal(200);
+      res.send.firstCall.args[0].status.should.equal(200);
+      res.send.firstCall.args[0].should.have.property('data');
+      res.send.firstCall.args[0].data.should.be.an('array');
+    });
+  });
+
+  describe('Create Meetup', () => {
+    describe('handle invalid user input', () => {
+      it('should not create meetup with incomplete data', () => {
+        const req = {
+          body: {}
+        };
+        const res = {
+          status() {},
+          send() {}
+        };
+
+        res.status = sinon.stub(res, 'status').returns(res);
+        res.send = sinon.stub(res, 'send').returns(res);
+        meetupController.createNewMeetup(req, res);
+
+        res.status.firstCall.args[0].should.equal(400);
+        res.send.firstCall.args[0].should.have.property('error');
+        res.send.firstCall.args[0].error.should.deep.equal('The topic, location and happeningOn fields are required fields');
+      });
+
+      it('should not create meetup with incomplete data', () => {
+        const req = {
+          body: {
+            topic: 'Sample Meetup',
+          }
+        };
+        const res = {
+          status() {},
+          send() {}
+        };
+
+        res.status = sinon.stub(res, 'status').returns(res);
+        res.send = sinon.stub(res, 'send').returns(res);
+        meetupController.createNewMeetup(req, res);
+
+        res.status.firstCall.args[0].should.equal(400);
+        res.send.firstCall.args[0].should.have.property('error');
+        res.send.firstCall.args[0].error.should.deep.equal('The topic, location and happeningOn fields are required fields');
+      });
+
+      it('should not create meetup with data of the wrong type', () => {
+        const req = {
+          body: {
+            topic: 'Sample Meetup',
+            location: 'Sample Location',
+            happeningOn: 'Tomorrow' // wrong data type
+          }
+        };
+        const res = {
+          status() {},
+          send() {}
+        };
+
+        res.status = sinon.stub(res, 'status').returns(res);
+        res.send = sinon.stub(res, 'send').returns(res);
+        meetupController.createNewMeetup(req, res);
+
+        res.status.firstCall.args[0].should.equal(422);
+        res.send.firstCall.args[0].should.have.property('error');
+        res.send.firstCall.args[0].error.should.deep.equal('You provided an invalid meetup date ');
+      });
     });
   });
 });
