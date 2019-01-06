8ca71fee666663ecf48008b34ec5557458020b21
Micah Akpan <mickey4sure2k7@gmail.com>
2019-01-06 17:38:06 +0100

    Add switch tab functionality

--- UI/js/profile.js
+++ UI/js/profile.js
@@ -1,3 +1,15 @@
-function uploadImage(container, img) {
-  
-}
+/* eslint-disable */
+
+const tabListItems = document.querySelectorAll('.nav__profile-tablist li');
+
+
+tabListItems.forEach((listItem) => {
+  listItem.onclick = () => {
+    tabListItems.forEach((item) => {
+      item.removeAttribute('class');
+    });
+
+    listItem.setAttribute('class', 'active');
+  }
+});
+
