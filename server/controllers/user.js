080d1d09ae97b55414b8fdd419efe2837b795874
Micah Akpan <mickey4sure2k7@gmail.com>
2019-01-08 11:06:40 +0100

    Refactor route handlers

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -22,31 +22,31 @@ export default {
           status: 422,
           error: 'A user with this email already exist'
         });
-      } else {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const createNewUserQuery = {
-          text: `INSERT INTO Users(email,password,firstname,lastname)
-                         VALUES ($1, $2, $3, $4) RETURNING *`,
-          values: [email, hashedPassword, firstname, lastname]
-        };
-
-        const userAuthToken = jwt.sign({ email }, process.env.JWT_SECRET, {
-          expiresIn: '24h'
-        });
-
-        const newTableResult = await db.queryDb(createNewUserQuery);
-        res.status(201).send({
-          status: 201,
-          data: [{
-            token: userAuthToken,
-            user: omitProps(newTableResult.rows[0], ['password'])
-          }]
-        });
       }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const createNewUserQuery = {
+        text: `INSERT INTO Users(email,password,firstname,lastname)
+                         VALUES ($1, $2, $3, $4) RETURNING *`,
+        values: [email, hashedPassword, firstname, lastname]
+      };
+
+      const userAuthToken = jwt.sign({ email }, process.env.JWT_SECRET, {
+        expiresIn: '24h'
+      });
+
+      const newTableResult = await db.queryDb(createNewUserQuery);
+      return res.status(201).send({
+        status: 201,
+        data: [{
+          token: userAuthToken,
+          user: omitProps(newTableResult.rows[0], ['password'])
+        }]
+      });
     } catch (e) {
       res.status(400).send({
         status: 400,
-        error: e.toString()
+        error: 'Invalid request, please check your email and try again'
       });
     }
   },
@@ -73,7 +73,7 @@ export default {
         const match = await bcrypt.compare(password, encryptedpassword);
 
         if (match) {
-          res.status(200)
+          return res.status(200)
             .send({
               status: 200,
               data: [{
@@ -83,14 +83,13 @@ export default {
                 user: omitProps(userResult.rows[0], ['password'])
               }]
             });
-        } else {
-          throw new Error('You entered an incorrect password, please check and try again');
         }
+        throw new Error('You entered an incorrect password, please check and try again');
       } else {
         throw new Error('A user with this email does not exist. Please check and try again. you can create an account at: http://localhost:9999/api/v2/auth/signup');
       }
     } catch (e) {
-      res.status(422)
+      return res.status(422)
         .send({
           status: 422,
           error: e.message
