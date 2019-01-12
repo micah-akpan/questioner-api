d44afb9b1b71a883086d4b4c06fb0eacf550f2b9
Micah Akpan <mickey4sure2k7@gmail.com>
2019-01-12 05:03:00 +0100

    [Feature #163083232] Add more tests

--- server/test/unit/db/db.spec.js
+++ server/test/unit/db/db.spec.js
@@ -1,17 +1,20 @@
 import 'chai/register-should';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import Db from '../../../db';
+import db from '../../../db';
 
 chai.use(chaiAsPromised);
 
 describe.only('DB', () => {
   describe('Drop table', () => {
+    before(async () => {
+      await db.createTable('User');
+    });
     it('can drop tables', () => {
-      Db.dropTable({
+      db.dropTable({
         tableName: '"User"',
         force: false
-      }).should.eventually.be.true;
+      }).should.eventually.have.property('rows');
     });
   });
 });
