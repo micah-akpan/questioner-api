0d7d6e0d86c3369c9957b89afbf8818b4612cf2e
Micah Akpan <mickey4sure2k7@gmail.com>
2019-01-17 14:39:34 +0100

    [Chore #163208486] Add data schema validation to login route

--- server/middlewares/schema/schemas/index.js
+++ server/middlewares/schema/schemas/index.js
@@ -3,10 +3,14 @@ import questionSchema from './question';
 import rsvpSchema from './rsvp';
 import userSchema from './user';
 
+const loginUserSchema = userSchema.getUserDataSchema('login');
+const signUpUserSchema = userSchema.getUserDataSchema('sign-up');
+
 export default {
   '/meetups': meetupSchema,
   '/questions': questionSchema,
   '/meetups/:meetupId/rsvps': rsvpSchema,
   '/meetups/:meetupId/rsvps/:rsvpId': rsvpSchema,
-  '/auth/signup': userSchema
+  '/auth/signup': signUpUserSchema,
+  '/auth/login': loginUserSchema
 };
--- server/middlewares/schema/schemas/user.js
+++ server/middlewares/schema/schemas/user.js
@@ -1,10 +1,18 @@
 const Joi = require('joi');
 
-const userDataSchema = Joi.object().keys({
-  email: Joi.string().strict().required(),
-  password: Joi.string().min(7).required(),
-  firstname: Joi.string().required(),
-  lastname: Joi.string()
-});
-
-export default userDataSchema;
+export default {
+  getUserDataSchema(type) {
+    if (type === 'sign-up') {
+      return Joi.object().keys({
+        email: Joi.string().strict().required(),
+        password: Joi.string().min(7).required(),
+        firstname: Joi.string().required(),
+        lastname: Joi.string()
+      });
+    }
+    return Joi.object().keys({
+      email: Joi.string().strict().required(),
+      password: Joi.string().min(7).required().trim(),
+    });
+  }
+};
