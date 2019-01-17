1bcce9fe812ad86a979116dec3e732e3f97e7786
Micah Akpan <mickey4sure2k7@gmail.com>
2019-01-17 13:35:01 +0100

    [Chore #163208486] Use more user-friendly error message

--- server/middlewares/schema/schemaValidator.js
+++ server/middlewares/schema/schemaValidator.js
@@ -38,17 +38,26 @@ export default (useJoiError = false) => {
               message: message.replace(/['"]/g, ''),
             }));
 
-
             let errorMsg = ' ';
 
-            details.forEach((detail) => {
+            // Forms a user friendly validation error
+            // message
+            details.forEach((detail, i) => {
               errorMsg += detail.message;
-              errorMsg += ', ';
+              if (i === details.length - 2) {
+                errorMsg += ' and ';
+              } else if (i === details.length - 1) {
+                errorMsg += '';
+              } else {
+                errorMsg += ', ';
+              }
             });
+
+            errorMsg = errorMsg.trim();
 
             const CustomError = {
               status: 422,
-              error: `Invalid request data: ${errorMsg} please review request and try again.`
+              error: errorMsg
             };
 
             res.status(422).send(_useJoiError ? JoiError : CustomError);
