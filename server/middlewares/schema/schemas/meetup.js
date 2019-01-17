97c978324812761f88f9a12ca5d865a6eb7cfa52
Micah Akpan <mickey4sure2k7@gmail.com>
2019-01-17 16:47:11 +0100

    [Chore #163208486] Relax iso date schema restriction

--- server/middlewares/schema/schemas/meetup.js
+++ server/middlewares/schema/schemas/meetup.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 const meetupDataSchema = Joi.object().keys({
   topic: Joi.string().strict().required(),
-  happeningOn: Joi.date().iso().required().min('now'),
+  happeningOn: Joi.date().required().min('now'),
   location: Joi.string().required(),
   tags: Joi.string(),
   images: Joi.array()
