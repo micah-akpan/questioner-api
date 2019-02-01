94a3321a31de1a56756f54681c6f498865e2cf3b
Micah Akpan <mickey4sure2k7@gmail.com>
2019-02-01 09:51:37 +0100

    [Chore #163239568] Refactor all Rsvp Controller functions

--- server/controllers/rsvp.js
+++ server/controllers/rsvp.js
@@ -1,5 +1,6 @@
 import db from '../db';
 import { Meetup, Rsvp } from '../models/all';
+import { sendResponse, sendServerErrorResponse } from './helpers';
 
 export default {
   async makeRsvp(req, res) {
@@ -14,12 +15,14 @@ export default {
         const rsvps = await Rsvp.find({ where: { '"user"': userId } });
 
         if (rsvps.length) {
-          // user already rsvped for this meetup
-          return res.status(409)
-            .send({
+          return sendResponse({
+            res,
+            status: 409,
+            payload: {
               status: 409,
               error: 'You have already rsvped for this meetup'
-            });
+            }
+          });
         }
         const rsvpQueryResult = await db.queryDb({
           text: `INSERT INTO Rsvp ("user", meetup, response)
@@ -35,25 +38,25 @@ export default {
           topic,
           status: rsvp.response
         };
-
-        return res.status(201)
-          .send({
+        return sendResponse({
+          res,
+          status: 201,
+          payload: {
             status: 201,
             data: [newRsvp]
-          });
+          }
+        });
       }
-
-      return res.status(404)
-        .send({
+      return sendResponse({
+        res,
+        status: 404,
+        payload: {
           status: 404,
           error: 'The requested meetup does not exist'
-        });
+        }
+      });
     } catch (e) {
-      return res.status(500)
-        .send({
-          status: 500,
-          error: 'Invalid request, please check request and try again'
-        });
+      return sendServerErrorResponse(res);
     }
   },
 
@@ -62,103 +65,107 @@ export default {
       const { meetupId, rsvpId } = req.params;
       const { userId } = req.decodedToken || req.body;
 
-      const results = await db.queryDb({
-        text: `SELECT * FROM Rsvp 
-                 WHERE id=$1 AND meetup=$2 AND "user"=$3`,
-        values: [rsvpId, meetupId, userId]
+      const rsvps = await Rsvp.find({
+        where: {
+          id: rsvpId,
+          meetup: meetupId,
+          '"user"': userId
+        }
       });
 
-      const rsvpRecord = results.rows[0];
+      const rsvp = rsvps[0];
 
-      if (rsvpRecord) {
-        const updatedRsvp = await db.queryDb({
+      if (rsvp) {
+        const updatedRsvpQueryResult = await db.queryDb({
           text: `UPDATE Rsvp
                    SET response=$1
                    WHERE id=$2 RETURNING *`,
-          values: [req.body.response, rsvpRecord.id]
+          values: [req.body.response, rsvp.id]
         });
-        return res.status(200)
-          .send({
-            status: 200,
-            data: [updatedRsvp.rows[0]]
-          });
-      }
 
-      return res.status(404)
-        .send({
+        const updatedRsvp = updatedRsvpQueryResult.rows[0];
+        return sendResponse({
+          res,
+          status: 200,
+          payload: {
+            status: 200,
+            data: [updatedRsvp]
+          }
+        });
+      }
+      return sendResponse({
+        res,
+        status: 404,
+        payload: {
           status: 404,
-          error: 'The requested rsvp does not exist for this user'
-        });
+          error: 'You have not rsvped for this meetup'
+        }
+      });
     } catch (e) {
-      return res.status(500)
-        .send({
-          status: 500,
-          error: 'Invalid request, please check request and try again'
-        });
+      return sendServerErrorResponse(res);
     }
   },
 
   async getRsvps(req, res) {
     try {
-      const results = await db.queryDb({
-        text: 'SELECT * FROM Rsvp WHERE meetup=$1',
-        values: [req.params.meetupId]
-      });
-
-      const rsvps = results.rows;
-
+      const rsvps = await Rsvp.find({ where: { meetup: req.params.meetupId } });
       if (rsvps.length > 0) {
-        return res.status(200)
-          .send({
+        return sendResponse({
+          res,
+          status: 200,
+          payload: {
             status: 200,
             data: rsvps
-          });
+          }
+        });
       }
-
-      return res.status(404)
-        .send({
+      return sendResponse({
+        res,
+        status: 404,
+        payload: {
           status: 404,
           error: 'There are no RSVPs for this meetup at the moment'
-        });
+        }
+      });
     } catch (e) {
-      return res.status(500)
-        .send({
-          status: 500,
-          error: 'Invalid request, please check request and try again'
-        });
+      return sendServerErrorResponse(res);
     }
   },
 
   async getRsvp(req, res) {
     try {
       const { meetupId, rsvpId } = req.params;
-      const results = await db.queryDb({
-        text: `SELECT * FROM Rsvp
-               WHERE id=$1 AND meetup=$2`,
-        values: [rsvpId, meetupId]
+
+      const rsvps = await Rsvp.find({
+        where: {
+          id: rsvpId,
+          meetup: meetupId
+        }
       });
 
-      const rsvpRecord = results.rows[0];
+      const rsvp = rsvps[0];
 
-      if (rsvpRecord) {
-        return res.status(200)
-          .send({
+      if (rsvp) {
+        return sendResponse({
+          res,
+          status: 200,
+          payload: {
             status: 200,
-            data: [rsvpRecord]
-          });
+            data: [rsvp]
+          }
+        });
       }
 
-      return res.status(404)
-        .send({
+      return sendResponse({
+        res,
+        status: 404,
+        payload: {
           status: 404,
-          error: `The rsvp with the id: ${req.params.rsvpId} for meetup with the id: ${req.params.meetupId} does not exist`
-        });
+          error: 'The rsvp for the requested meetup does not exist'
+        }
+      });
     } catch (e) {
-      return res.status(500)
-        .send({
-          status: 500,
-          error: 'Invalid request, please check request and try again'
-        });
+      return sendServerErrorResponse(res);
     }
   }
 };
